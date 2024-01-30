0928565e4802bf7e60d41da4a8d24475ebbdd587
David Kerr <kerrky@gmail.com>
2024-01-29 22:09:15 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+
+function saturdayFun(string) {
+  if(string) {
+  return `This Saturday, I want to ${string}!`;
+  } else if(!string) {
+    return 'This Saturday, I want to roller-skate!';
+  }
+}
+
+function mondayWork(string) {
+  if(string) {
+    return `This Monday, I will ${string}.`;
+  } else if(!string) {
+    return 'This Monday, I will go to the office.';
+  }
+}
+
+function wrapAdjective(string) {
+  if(string === '*') {
+    return function() {
+      return `You are ${string}a hard worker${string}!`;
+    }
+  } else if(string === '||') {
+    return function() {
+      return `You are ${string}a dedicated programmer${string}!`;
+    }
+  }
+}
+
